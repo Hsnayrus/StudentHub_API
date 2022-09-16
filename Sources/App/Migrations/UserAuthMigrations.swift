726ca6c89b9e60e61a2ca9fe3e1d36a8c683b79e
hsnayrus <your_email@gmail.com>
2022-09-16 15:01:41 -0400

    Major changes to UserAuth controller, model. Added new routes, updated readme, etc.

--- Sources/App/Migrations/UserAuthMigrations.swift
+++ Sources/App/Migrations/UserAuthMigrations.swift
@@ -10,13 +10,22 @@ import Vapor
 
 struct UserAuthMigrations: AsyncMigration{
     func prepare(on database: Database) async throws{
+        
+        let userTypeDBEnum = try await database.enum("user_type")
+            .case("Professor")
+            .case("TA")
+            .case("Student")
+            .create()
+        
         try await database.schema("ece564server_user_auth")
             .id()
             .field("username", .string, .required)
             .field("password", .string)
+            .field("user_type", userTypeDBEnum, .required)
             .unique(on: "username")
             .create()
-        let AdminUser = UserAuth(id: UUID(), username: "rt113", password: UUID().uuidString)
+        
+        let AdminUser = UserAuth(id: UUID(), username: "rt113", password: "ece564server_vapor", userType: UserType.Professor)
         try await AdminUser.create(on: database)
     }
     
--- Sources/App/Models/UserAuth.swift
+++ Sources/App/Models/UserAuth.swift
@@ -8,6 +8,12 @@
 import Fluent
 import Vapor
 
+enum UserType: String, CodableC{
+    case Professor
+    case TA
+    case Student
+}
+
 final class UserAuth: Model, Content{
     static let schema: String = "ece564server_user_auth"
     
@@ -20,13 +26,25 @@ final class UserAuth: Model, Content{
     @OptionalField(key: "password")
     var password: String?
     
+    @Enum(key: "user_type")
+    var userType: UserType
+    
     init(){
         
     }
     
-    init(id: UUID? = nil, username: String, password: String){
+    init(id: UUID? = nil, username: String, password: String? = nil, userType: UserType){
         self.id = id
         self.username = username
+        if password != nil{
+            self.password = password
+        }
+        else{
+            let usernameData = Data(self.username.utf8)
+            let usernameHashed = SHA256.hash(data: usernameData)
+            self.password = usernameHashed.hex
+        }
         self.password = password
+        self.userType = userType
     }
 }
